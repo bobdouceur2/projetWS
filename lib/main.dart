f5a25997789ac8fb24ac4ab0a4914c97f0ce6504
brian <collo.brian@orange.fr>
2024-03-15 09:00:35 +0100

    letsgooooo

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:fl_chart/fl_chart.dart';
 
+// Définition des couleurs de la palette
+const Color color1 = Color(0xFFE7ECEF);
+const Color color2 = Color(0xFF274C77);
+const Color color3 = Color(0xFF6096BA);
+const Color color4 = Color(0xFFA3CEF1);
+
 void main() {
   runApp(MyApp());
 }
@@ -11,7 +17,8 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Tableau de Bord - Musées de Paris',
       theme: ThemeData(
-        primarySwatch: Colors.blueGrey,
+        primaryColor: color2,
+        scaffoldBackgroundColor: color1,
         fontFamily: 'Arial',
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
@@ -27,7 +34,7 @@ class DashboardScreen extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         title: Text('Musées-de-Paris.Stats'),
-        backgroundColor: Colors.blueGrey,
+        backgroundColor: color2,
       ),
       body: SingleChildScrollView(
         child: Padding(
@@ -38,22 +45,52 @@ class DashboardScreen extends StatelessWidget {
               SizedBox(height: 20),
               Text(
                 "Musée du Louvres",
-                style: Theme.of(context).textTheme.headline5?.copyWith(color: Colors.black54),
+                style: Theme.of(context).textTheme.headline5?.copyWith(color: color2),
               ),
               SizedBox(height: 20),
               AffluenceBarChart(),
-              StatisticCard(title: "Nombre de visiteurs en 2023", value: "9,7 millions"),
-              StatisticCard(title: "Nombre d'œuvres", value: "35 000"),
-              StatisticCard(title: "Nombre de salles", value: "380"),
+              StatisticCard(
+                title: "Nombre de visiteurs aujourd'hui",
+                value: "2,300",
+                onTap: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => VisitorDetailPage()),
+                  );
+                },
+                color: color4,
+              ),
+              StatisticCard(
+                title: "Nombre d'œuvres",
+                value: "35 000",
+                onTap: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => ArtworksDetailPage()),
+                  );
+                },
+                color: color4,
+              ),
+              StatisticCard(
+                title: "Nombre de salles",
+                value: "380",
+                onTap: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => RoomsDetailPage()),
+                  );
+                },
+                color: color4,
+              ),
               SizedBox(height: 20),
               Text(
                 "Explorer d'autres musées",
-                style: Theme.of(context).textTheme.headline6?.copyWith(color: Colors.black54),
+                style: Theme.of(context).textTheme.headline6?.copyWith(color: color2),
               ),
               SizedBox(height: 10),
-              DashboardButton(title: 'Musée du Quai Branly', context: context),
-              DashboardButton(title: "Musée de l'Orangerie", context: context),
-              DashboardButton(title: 'Musée Guimet', context: context),
+              DashboardButton(title: 'Musée du Quai Branly', context: context, destinationPage: QuaiBranlyDetailPage()),
+              DashboardButton(title: "Musée de l'Orangerie", context: context, destinationPage: OrangerieDetailPage()),
+              DashboardButton(title: 'Musée Guimet', context: context, destinationPage: GuimetDetailPage()),
             ],
           ),
         ),
@@ -65,21 +102,27 @@ class DashboardScreen extends StatelessWidget {
 class StatisticCard extends StatelessWidget {
   final String title;
   final String value;
+  final VoidCallback onTap;
+  final Color color;
 
-  const StatisticCard({Key? key, required this.title, required this.value}) : super(key: key);
+  const StatisticCard({Key? key, required this.title, required this.value, required this.onTap, required this.color}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Card(
-      elevation: 4,
-      margin: EdgeInsets.symmetric(vertical: 10),
-      shape: RoundedRectangleBorder(
-        borderRadius: BorderRadius.circular(15),
-      ),
-      child: ListTile(
-        contentPadding: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
-        title: Text(title, style: TextStyle(fontSize: 18)),
-        trailing: Text(value, style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold, color: Colors.blueGrey)),
+    return InkWell(
+      onTap: onTap,
+      child: Card(
+        color: color,
+        elevation: 4,
+        margin: EdgeInsets.symmetric(vertical: 10),
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(15),
+        ),
+        child: ListTile(
+          contentPadding: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+          title: Text(title, style: TextStyle(fontSize: 18, color: color2)),
+          trailing: Text(value, style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold, color: color3)),
+        ),
       ),
     );
   }
@@ -88,8 +131,9 @@ class StatisticCard extends StatelessWidget {
 class DashboardButton extends StatelessWidget {
   final String title;
   final BuildContext context;
+  final Widget destinationPage;
 
-  const DashboardButton({Key? key, required this.title, required this.context}) : super(key: key);
+  const DashboardButton({Key? key, required this.title, required this.context, required this.destinationPage}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -97,18 +141,111 @@ class DashboardButton extends StatelessWidget {
       width: double.infinity,
       padding: EdgeInsets.symmetric(vertical: 5),
       child: ElevatedButton(
-        style: ElevatedButton.styleFrom(
-          foregroundColor: Colors.white,
-          backgroundColor: Colors.blueGrey,
-          shape: RoundedRectangleBorder(
-            borderRadius: BorderRadius.circular(30),
-          ),
-          padding: EdgeInsets.symmetric(vertical: 15),
-        ),
-        onPressed: () {
-          // Implémentez la navigation vers les écrans correspondants
-        },
+      style: ElevatedButton.styleFrom(
+      foregroundColor: color1,
+      backgroundColor: color2,
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(30),
+      ),
+      padding: EdgeInsets.symmetric(vertical: 15),
+    ),
+    onPressed: () {
+    Navigator.push(
+      context,
+      MaterialPageRoute(builder: (context) => destinationPage),
+    );
+    },
         child: Text(title),
+      ),
+    );
+  }
+}
+
+class VisitorDetailPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Détails des visiteurs"),
+        backgroundColor: color2,
+      ),
+      body: Center(
+        child: Text("Plus d'informations sur les visiteurs ici."),
+      ),
+    );
+  }
+}
+
+class ArtworksDetailPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Détails des œuvres"),
+        backgroundColor: color2,
+      ),
+      body: Center(
+        child: Text("Plus d'informations sur les œuvres ici."),
+      ),
+    );
+  }
+}
+
+class RoomsDetailPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Détails des salles"),
+        backgroundColor: color2,
+      ),
+      body: Center(
+        child: Text("Plus d'informations sur les salles ici."),
+      ),
+    );
+  }
+}
+
+class QuaiBranlyDetailPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Musée du Quai Branly"),
+        backgroundColor: color2,
+      ),
+      body: Center(
+        child: Text("Détails du Musée du Quai Branly ici."),
+      ),
+    );
+  }
+}
+
+class OrangerieDetailPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Musée de l'Orangerie"),
+        backgroundColor: color2,
+      ),
+      body: Center(
+        child: Text("Détails du Musée de l'Orangerie ici."),
+      ),
+    );
+  }
+}
+
+class GuimetDetailPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Musée Guimet"),
+        backgroundColor: color2,
+      ),
+      body: Center(
+        child: Text("Détails du Musée Guimet ici."),
       ),
     );
   }
@@ -118,55 +255,58 @@ class AffluenceBarChart extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      height: 200,
-      padding: const EdgeInsets.symmetric(horizontal: 20.0, vertical: 10),
-      child: BarChart(
+        height: 200,
+        padding: const EdgeInsets.symmetric(horizontal: 20.0, vertical: 10),
+        child: BarChart(
         BarChartData(
-          alignment: BarChartAlignment.spaceAround,
-          maxY: 20,
-          barTouchData: BarTouchData(enabled: false),
-          titlesData: FlTitlesData(
-            show: true,
-            bottomTitles: SideTitles(
-              showTitles: true,
-              getTextStyles: (context, value) => const TextStyle(color: Colors.black, fontWeight: FontWeight.bold, fontSize: 14),
-              margin: 10,
-              getTitles: (double value) {
-                switch (value.toInt()) {
-                  case 0:
-                    return '10h';
-                  case 1:
-                    return '11h';
-                  case 2:
-                    return '12h';
-                  case 3:
-                    return '13h';
-                  case 4:
-                    return '14h';
-                  case 5:
-                    return '15h';
-                  case 6:
-                    return '16h';
-                  default:
-                    return '';
-                }
-              },
-            ),
-            leftTitles: SideTitles(showTitles: false),
+        alignment: BarChartAlignment.spaceAround,
+        maxY: 20,
+        barTouchData: BarTouchData(enabled: false),
+        titlesData: FlTitlesData(
+          show: true,
+          bottomTitles: SideTitles(
+            showTitles: true,
+            getTextStyles: (context, value) => TextStyle(color: color2, fontWeight: FontWeight.bold, fontSize: 14),
+            margin: 10,
+            getTitles: (double value) {
+              switch (value.toInt()) {
+                case 0:
+                  return '10h';
+                case 1:
+                  return '11h';
+                case 2:
+                  return '12h';
+                case 3:
+                  return '13h';
+                case 4:
+                  return '14h';
+                case 5:
+                  return '15h';
+                case 6:
+                  return '16h';
+                default:
+                  return '';
+              }
+            },
           ),
-          gridData: FlGridData(show: false),
-          borderData: FlBorderData(show: false),
-          barGroups: [
-            BarChartGroupData(x: 0, barRods: [BarChartRodData(y: 8, width: 15)]),
-            BarChartGroupData(x: 1, barRods: [BarChartRodData(y: 10, width: 15)]),
-            BarChartGroupData(x: 2, barRods: [BarChartRodData(y: 14, width: 15)]),
-            BarChartGroupData(x: 3, barRods: [BarChartRodData(y: 15, width: 15)]),
-            BarChartGroupData(x: 4, barRods: [BarChartRodData(y: 13, width: 15)]),
-            BarChartGroupData(x: 5, barRods: [BarChartRodData(y: 10, width: 15)]),
-            BarChartGroupData(x: 6, barRods: [BarChartRodData(y: 5, width: 15)]),
-          ],
+          leftTitles: SideTitles(showTitles: false),
         ),
-      ),
+        gridData: FlGridData(show: false),
+        borderData: FlBorderData(show: false),
+        barGroups: [
+        BarChartGroupData(x: 0, barRods: [BarChartRodData(y: 8, colors: [color3], width: 15)]),
+    BarChartGroupData(x: 1, barRods: [BarChartRodData(y: 10, colors: [color3], width: 15)]),
+    BarChartGroupData(x: 2, barRods: [BarChartRodData(y: 14, colors: [color3], width: 15)]),
+    BarChartGroupData(x: 3, barRods: [BarChartRodData(y: 15, colors: [color3], width: 15)]),
+    BarChartGroupData(x: 4, barRods: [BarChartRodData(y: 13, colors: [color3], width: 15)]),
+          BarChartGroupData(x: 5, barRods: [BarChartRodData(y: 10, colors: [color3], width: 15)]),
+          BarChartGroupData(x: 6, barRods: [BarChartRodData(y: 5, colors: [color3], width: 15)]),
+        ],
+        ),
+        ),
     );
   }
 }
+
+
+
