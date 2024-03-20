45e405ae6f76dc021becb54f8100bf62abd71b45
brian <collo.brian@orange.fr>
2024-03-20 11:24:58 +0100

    ajouts fichiers pour separer differents horaires, pages d'accueil principales et pages avec images pour les differents musées

--- lib/DashboardScreen.dart
+++ lib/DashboardScreen.dart
@@ -13,6 +13,8 @@ import 'main.dart';
 class DashboardScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    double screenWidth = MediaQuery.of(context).size.width;
+
     return Scaffold(
       appBar: AppBar(
         title: Text(
@@ -21,65 +23,74 @@ class DashboardScreen extends StatelessWidget {
         ),
         backgroundColor: color2,
       ),
-      body: SingleChildScrollView(
-        child: Padding(
-          padding: const EdgeInsets.all(20.0),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: <Widget>[
-              SizedBox(height: 20),
-              Text(
-                "Musée du Louvre",
-                style: Theme.of(context).textTheme.headline5?.copyWith(color: color2),
-              ),
-              SizedBox(height: 20),
-              AffluenceBarChart(),
-
-              StatisticCard(
-                title: "Aperçu des œuvres exposées",
-
-
-                onTap: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(builder: (context) => ArtworksGalleryPage()),
-                  );
-                },
-                color: color4,
-              ),
-              // Bouton "Nombre de visiteurs aujourd'hui" déplacé en deuxième position
-              StatisticCard(
-                title: "Horaires d'ouverture détaillés",
-
-                onTap: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(builder: (context) => HorairesLouvre()),
-                  );
-                },
-                color: color4,
-              ),
-              StatisticCard(
-                title: "Prix des Tickets",
-
-                onTap: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(builder: (context) => PrixTicketLouvre()),
-                  );
-                },
-                color: color4,
-              ),
-              SizedBox(height: 20),
-              Text(
-                "Explorer d'autres musées",
-                style: Theme.of(context).textTheme.headline6?.copyWith(color: color2),
-              ),
-              SizedBox(height: 10),
-              DashboardButton(title: 'Musée du Quai Branly', context: context, destinationPage: QuaiBranlyDetailPage()),
-              DashboardButton(title: "Musée de l'Orangerie", context: context, destinationPage: OrangerieDetailPage()),
-              DashboardButton(title: 'Musée Guimet', context: context, destinationPage: GuimetDetailPage()),
-            ],
+      body: Container(
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage("interior-wood-Sony-museum-interior-design-estate-427325-wallhere.com.jpg"), // Remplacez par le chemin de votre image
+            fit: BoxFit.cover,
+          ),
+        ),
+        child: SingleChildScrollView(
+          child: Padding(
+            padding: const EdgeInsets.all(20.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                SizedBox(height: 20),
+                Text(
+                  "Musée du Louvre",
+                  style: Theme.of(context).textTheme.headline5?.copyWith(color: color2),
+                ),
+                SizedBox(height: 20),
+                AffluenceBarChart(),
+                GridView.count(
+                  shrinkWrap: true,
+                  crossAxisCount: screenWidth > 800 ? 3 : 1,
+                  childAspectRatio: 4,
+                  children: <Widget>[
+                    StatisticCard(
+                      title: "Aperçu des œuvres exposées",
+                      onTap: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(builder: (context) => ArtworksGalleryPage()),
+                        );
+                      },
+                      color: color4,
+                    ),
+                    StatisticCard(
+                      title: "Horaires d'ouverture détaillés",
+                      onTap: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(builder: (context) => HorairesLouvre()),
+                        );
+                      },
+                      color: color4,
+                    ),
+                    StatisticCard(
+                      title: "Prix des Tickets",
+                      onTap: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(builder: (context) => PrixTicketLouvre()),
+                        );
+                      },
+                      color: color4,
+                    ),
+                  ],
+                ),
+                SizedBox(height: 20),
+                Text(
+                  "Explorer d'autres musées",
+                  style: Theme.of(context).textTheme.headline6?.copyWith(color: color2),
+                ),
+                SizedBox(height: 10),
+                DashboardButton(title: 'Musée du Quai Branly', context: context, destinationPage: QuaiBranlyDetailPage()),
+                DashboardButton(title: "Musée de l'Orangerie", context: context, destinationPage: OrangerieDetailPage()),
+                DashboardButton(title: 'Musée Guimet', context: context, destinationPage: GuimetDetailPage()),
+              ],
+            ),
           ),
         ),
       ),
